b3a8ba82f8a993e85baa0024eb779e1ebb5ff5b9
Mario Lamacchia <mariolamacchia@gmail.com>
2015-04-09 11:01:24 +0200

    Add some routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,8 +21,25 @@ angular
     .config(function ($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
+                templateUrl: 'views/dashboard.html'
+            })
+            .when('/dashboard', {
+                templateUrl: 'views/dashboard.html'
+            })
+            .when('/charts_xcharts', {
+                templateUrl: 'views/charts_xcharts.html'
+            })
+            .when('/charts_float', {
+                templateUrl: 'views/charts_float.html'
+            })
+            .when('/charts_float', {
+                templateUrl: 'views/charts_float.html'
+            })
+            .when('/charts_google', {
+                templateUrl: 'views/charts_google.html'
+            })
+            .when('/charts_coindesk', {
+                templateUrl: 'views/charts_coindesk.html'
             })
             .otherwise({
                 redirectTo: '/'
