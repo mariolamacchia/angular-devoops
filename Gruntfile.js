543081b8248069e0e9be670a3776eb2180d873c3
Mario Lamacchia <mariolamacchia@gmail.com>
2015-04-12 21:03:38 +0200

    Gruntfile: move fontawesome fonts to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -327,6 +327,11 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: 'bower_components/fontawesome',
+          src: 'fonts/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
